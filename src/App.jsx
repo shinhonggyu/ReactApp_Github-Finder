8105dde9b93d408fc6de87f9e0def56d0f19f883
honggyu <zowoz0@naver.com>
2020-11-03 19:12:52 +0900

    Users & UserItem components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Fuck</h1>
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
