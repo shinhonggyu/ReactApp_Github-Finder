95831fa881680326623b0692ceeae5b591cb7c19
honggyu <zowoz0@naver.com>
2020-11-04 19:01:51 +0900

    Clear Users From State

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
   state = {
     text: '',
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
 
   onChange = (event) =>
@@ -10,16 +17,20 @@ class Search extends Component {
 
   onSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
           <input
             type="text"
             name="text"
+            value={this.state.text}
             placeholder="Search Users..."
             onChange={this.onChange}
           />
@@ -29,6 +40,11 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
