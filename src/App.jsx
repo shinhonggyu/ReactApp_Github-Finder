b6e80dc5bd21b197990c94e093bc921c926d3d92
honggyu <zowoz0@naver.com>
2020-11-04 19:51:55 +0900

    Alert State & Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // Search Github users
@@ -26,6 +28,14 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  setAlert = (message, type) => {
+    this.setState({ alert: { message: message, type: type } });
+
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 3000);
+  };
+
   render() {
     const { users, loading } = this.state;
 
@@ -33,10 +43,12 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
